41082cb8a7e52084cef4aefcfec036948eefb034
Wonwoo Seo <wonwooseo@hotmail.com>
2018-10-26 21:29:05 +0900

    multi-monitor via threading

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 Watch coin price and buy / sell on user-set point.
 Uses Coinone API.
 
+## Price Monitor
+Monitor for each currency works with 2 threads, one for GUI loop and one for 
+data fetching via Coinone API. Those threads communicate using a queue.
+Global flag is used to signal exit.
+
+## Transactions
+Not implemented yet.
+
 ## TODOs
 - [x] Price monitoring loop
 - [x] GUI to show monitoring results
+- [x] Multiple monitors via threading
+- [x] Config INI for monitor settings
 - [ ] In-GUI menu to change options of monitor
 - [ ] Send alert to user when preset price is met
 
--- watch_loop.py
+++ watch_loop.py
@@ -1,6 +1,7 @@
 import requests
 import threading
 import queue
+import configparser
 from datetime import datetime
 import time
 from tkinter import *
@@ -10,75 +11,137 @@ from tkinter import scrolledtext
 
 
 api_url = "https://api.coinone.co.kr/ticker/"
-# Options
-target_currency = 'btc'
-cool_down = 5
-# Exit when true
-exit_flag = False
-# Queue for thread communication
-q = queue.Queue()
+# Options from config
+config = configparser.ConfigParser()
+config.read("monitor_config.ini")
+targets = config['TARGET']['currency'].split('\n')[1:]
+cool_down = int(config['SETTINGS']['cooldown'])
 
 
-# monitoring loop at new thread
 class MonitorLoop(threading.Thread):
+    """
+    Thread for price monitor. Loops until flag turns and signals exit.
+    Interval between each loop can be adjusted from config file.
+    Fetches latest data of given currency via Coinone API and passes price
+    value to GUI Thread via queue.
+    """
+    def __init__(self, tid, target, q):
+        """
+        Overriden function to add arguments on thread initialization.
+        :param tid: (int) Thread ID
+        :param target: (string) Currency ticker
+        :param q: (queue.Queue) Queue object for thread communication
+        """
+        threading.Thread.__init__(self)
+        self.id = tid
+        self.currency = target
+        self.q = q
+
     def run(self):
-        while not exit_flag:
-            res = requests.get(api_url, params={'currency': target_currency})
+        """
+        Execution part of thread. Fetches latest data and pass it to GUI.
+        :return: no return value
+        """
+        while not flag_arr[self.id]:
+            res = requests.get(api_url, params={'currency': self.currency})
             price = res.json()['last']
             timestamp = int(res.json()['timestamp'])
-            q.put((price, timestamp)) # Bind as tuple and send to queue
+            self.q.put((price, timestamp))  # Bind as tuple and send to queue
             time.sleep(cool_down)  # halt for preset seconds
 
 
-# Console update function
-def console_update():
-    if not q.empty():
-        data_tup = q.get()
-        price = data_tup[0]
-        update_time = str(datetime.fromtimestamp(data_tup[1]))
-        outline = "[" + update_time + "] " + "Price for " + \
-                  target_currency + ": " + str(price) + '\n'
-        console_text['state'] = 'normal'
-        console_text.insert(END, outline)
-        console_text['state'] = 'disabled'
-        console_text.see(END)
-        window.after(5000, console_update)
-    else:
-        window.after(1000, console_update)
+class GUILoop(threading.Thread):
+    """
+    Thread for GUI. Picks up data sent from monitor thread through queue and
+    updates the textbox. tkinter main loop runs on this thread.
+    """
+    def __init__(self, tid, target, q):
+        """
+        Overriden function to add arguments on thread initialization.
+        :param tid: (int) Thread ID
+        :param target: (string) Currency ticker
+        :param q: (queue.Queue) Queue object for thread communication
+        """
+        threading.Thread.__init__(self)
+        self.id = tid
+        self.currency = target
+        self.q = q
+
+    def run(self):
+        """
+        Execution part of this thread. Sets up GUI and starts GUI main loop.
+        :return: no return value
+        """
+        def console_update():
+            """
+            Fetches latest price data from the queue and updates the textbox
+            of GUI. This function should be explicitly called using after
+            method of tkinter window.
+            :return: no return value
+            """
+            if not self.q.empty():
+                data_tup = self.q.get()
+                price = data_tup[0]
+                update_time = str(datetime.fromtimestamp(data_tup[1]))
+                outline = "[" + update_time + "] " + "Price for " + \
+                          self.currency + ": " + str(price) + '\n'
+                console_text['state'] = 'normal'
+                console_text.insert(END, outline)
+                console_text['state'] = 'disabled'
+                console_text.see(END)
+                window.after(5000, console_update)  # reserve next call
+            else:  # if queue was empty, wait for short time and check again
+                window.after(1000, console_update)
+
+        def sys_exit():
+            """
+            Callback function initiated when window closes or user exits from
+            menu. Joins monitor thread and exits current thread.
+            :return: no return value
+            """
+            exit_check = messagebox.askyesno("Exit", "Shutdown this monitor?")
+            if exit_check:
+                flag_arr[self.id] = True
+                # TODO: find other options for joining (delay before joining)
+                monitor_thread.join()  # to exit gracefully
+                sys.exit(0)  # quit current thread
+            else:
+                pass
+
+        # Start monitor loop thread
+        monitor_thread = MonitorLoop(self.id, self.currency, self.q)
+        monitor_thread.start()
+        # GUI on current thread
+        window = Tk()
+        window.title("Coinwatch: " + self.currency)
+        window.minsize(800, 450)
+        window.maxsize(800, 450)
+        # Menu bar settings
+        menu_bar = Menu(window)
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Exit..", command=sys_exit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        # Add options here
+        window.config(menu=menu_bar)
+        # Console tab
+        console_page = ttk.Frame(window)
+        console_text = scrolledtext.ScrolledText(console_page,
+                                                 state="disabled", wrap="none",
+                                                 bg="black", fg="lawn green")
+        console_text.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        console_page.pack(side=TOP, fill=BOTH, expand=True)
+        # Start text update function
+        window.after(0, console_update)
+        # Show message box when closing window
+        window.protocol("WM_DELETE_WINDOW", sys_exit)
+        window.mainloop()
 
 
-# GUI exit function
-def sys_exit():
-    exit_check = messagebox.askyesno("Exit", "Shutdown Coinwatch?")
-    if exit_check:
-        global exit_flag
-        exit_flag = True
-        monitor_thread.join()
-        sys.exit(0)
-    else:
-        pass
-
-
-# Start monitor loop thread
-monitor_thread = MonitorLoop()
-monitor_thread.start()
-# GUI on main thread
-window = Tk()
-window.title("Coinwatch")
-window.minsize(800, 450)
-window.maxsize(800, 450)
-# Menu bar settings
-menu_bar = Menu(window)
-file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Exit..", command=sys_exit)
-menu_bar.add_cascade(label="File", menu=file_menu)
-# Add options here
-window.config(menu=menu_bar)
-# Console tab
-console_page = ttk.Frame(window)
-console_text = scrolledtext.ScrolledText(console_page, state="disabled", wrap="none", bg="black", fg="lawn green")
-console_text.pack(fill=BOTH, expand=True, padx=5, pady=5)
-console_page.pack(side=TOP, fill=BOTH, expand=True)
-# Start text update function
-window.after(0, console_update)
-window.mainloop()
+flag_arr = []  # global list to store flags
+for i in range(0, len(targets)):
+    new_flag = False
+    flag_arr.append(new_flag)
+    new_q = queue.Queue()
+    new_thread = GUILoop(i, targets[i], new_q)
+    new_thread.start()
+    time.sleep(0.5)  # error prevention
